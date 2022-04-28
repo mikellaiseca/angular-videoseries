52c243eadaf0c27b7dc16ef91ab60ce0f336c959
mikellaiseca <mikellaiseca@gmail.com>
2022-04-28 12:08:08 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-yourstep';
+export class AppComponent implements OnInit, OnDestroy {
+  intervalSub: any;
+
+  ngOnInit(): void {
+    this.intervalSub = setInterval(() => {
+      console.log('Hello from ngOnInit');
+    }, 1000)
+  }
+
+  ngOnDestroy() {
+    if (this.intervalSub) {
+      clearInterval(this.intervalSub)
+    }
+  }
+
+  title = 'Hello World!'
+
+  getMin(a: number, b: number) {
+    if (a < b) {
+      return a
+    }
+
+    return b
+  }
+
 }
